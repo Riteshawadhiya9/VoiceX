658d370380f03d975a3745d68f36fa69a8ac0d14
Ritesh Awadhiya <riteshawadhiya2004@gmail.com>
2026-06-03 00:25:28 +0530

    Add main.jsx

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import UserContext from './contextApi/UserContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+
+  <BrowserRouter>
+    <UserContext>
+      <App />
+    </UserContext>
+  </BrowserRouter>
+
+)
